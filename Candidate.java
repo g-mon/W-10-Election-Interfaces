bcc77fc544330ede49e44ac5faac85cede6a4050
g-mon <33523530+g-mon@users.noreply.github.com>
2017-12-13 09:46:44 +0000

    Candidate Constituency classes

---
+++ Candidate.java
@@ -0,0 +1,15 @@
+/**
+public class Candidate implements CandidateInterface{
+    public Candidate(String candName){
+    }
+    
+    public PartyInterface getParty(){
+    }
+    public void setParty(PartyInterface party){
+    }
+    public String getName(){
+    }
+    public boolean isIndependent(){
+    }
+}
+**/
---
+++ Constituency.java
@@ -0,0 +1,33 @@
+/**
+import java.util.Set;
+public class Constituency implements ConstituencyInterface{
+    public Constituency(){
+    }
+    
+    public Set<CandidateInterface> getCandidates(){
+    }
+    
+    public void addCandidate(Candidateinterface candidate){
+    }
+    
+    public set<PollingStationInterface> getPollingStations(){
+    }
+    
+    public void addPollingStation(PollingStationInterface pollingStation){
+    }
+    
+    public String getName(){
+    }
+    
+    public void setName(String name){
+    }
+    
+    public int voteCount(CandidateInterface candidate){
+    }
+    
+    public CandidateInterface winner(){
+    }
+}
+**/
+
+    
