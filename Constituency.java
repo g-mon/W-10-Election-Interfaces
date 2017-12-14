9602d30206f2ef7f0d815172fdb82bdbc2aa5bc2
george monaghan <georgemonaghan@student-10-245-162-17.wlan.dur.ac.uk>
2017-12-14 11:51:23 +0000

    Billy B 14/12

--- Constituency.java
+++ Constituency.java
@@ -1,26 +1,33 @@
 /*
 import java.util.Set;
+import java.util.HashSet;
 public class Constituency implements ConstituencyInterface{
     private String conName;
+    private HashSet<CandidateInterface> candsInCon = new HashSet<CandidateInterface>();
+    private HashSet<PollingStationInterface> stasInCon = new HashSet<PollingStationInterface>();
     
     public Constituency(String conName){
         this.conName=conName;
     }
     
     public Set<CandidateInterface> getCandidates(){
+        return candsInCon;
     }
     
-    public void addCandidate(Candidateinterface candidate){
+    public void addCandidate(CandidateInterface candidate){
+        candsInCon.add(candidate);
     }
     
-    public set<PollingStationInterface> getPollingStations(){
+    public Set<PollingStationInterface> getPollingStations(){
+        return stasInCon;
     }
     
     public void addPollingStation(PollingStationInterface pollingStation){
+        stasInCon.add(pollingStation);
     }
     
     public String getName(){
-        return name;
+        return conName;
     }
     
     public void setName(String name){
